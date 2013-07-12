9ab0f0ecae95dfb90cc014fe1a608f8b222e20bd
Faisal Iqbal <iqbal.faisal788@gmail.com>
2013-07-12 06:39:46 +0000

    web.js

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var k = fs.readFileSync('./index.html','utf8');
-    response.end(k.toString() );
-});
+    response.send(k);
+};
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
